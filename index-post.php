5de672cb98085c2d45d09aad7365c4ddc6e6f616
Renzo Sarmiento Espinoza <104445632+renzosrmnt@users.noreply.github.com>
2024-04-05 11:43:29 -0500

    Update index-post.php

--- index-post.php
+++ index-post.php
@@ -8,7 +8,7 @@ $ape = $_POST["ape"];
 $dir = $_POST["dir"];
 $cel = $_POST["cel"];
 
-$sql = "insert into persona values(default,'$doc','$nom','$ape','$dir','$cel')";
+$sql = "insert into alumno values(default,'$doc','$nom','$ape','$dir','$cel')";
 pg_query($con, $sql);
 
 header("location:index.php");
