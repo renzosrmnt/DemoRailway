af89f372328f275f482bb27e67ec11bdc1e6550b
RENZO SARMIENTO <renzo.sarmiento@tecsup.edu.pe>
2024-04-11 10:52:51 -0500

    Listar Registros

---
+++ listar.php
@@ -0,0 +1,54 @@
+<?php
+include("conexion.php");
+$con = conexion();
+
+$sql = "SELECT * FROM alumno";
+$result = pg_query($con, $sql);
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <title>Lista de Registros</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/pricing/">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h2 class="mt-5 mb-4">Lista de Registros</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nro Documento</th>
+                    <th>Nombre</th>
+                    <th>Apellidos</th>
+                    <th>Dirección</th>
+                    <th>Celular</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($row = pg_fetch_assoc($result)) { ?>
+                    <tr>
+                        <td><?php echo $row['doc']; ?></td>
+                        <td><?php echo $row['nombre']; ?></td>
+                        <td><?php echo $row['apellidos']; ?></td>
+                        <td><?php echo $row['direccion']; ?></td>
+                        <td><?php echo $row['celular']; ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
